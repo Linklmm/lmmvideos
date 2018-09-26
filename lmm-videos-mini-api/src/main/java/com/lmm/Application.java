10e94dbc2e8cf926a5050caa64007957c620104c
lmm <15060335532@139.com>
2018-09-26 17:26:08 +0800

    提交

---
+++ lmm-videos-mini-api/src/main/java/com/lmm/Application.java
@@ -0,0 +1,21 @@
+/**
+ * @program: lmmvideos
+ * @description: 启动类
+ * @author: minmin.liu
+ * @create: 2018-09-26 16:17
+ **/
+package com.lmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.lmm.mapper")
+@ComponentScan(basePackages = {"com.lmm"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ lmm-videos-mini-api/src/main/java/com/lmm/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.lmm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public String Hello() {
+		return "Hello MyFlowerYourGrass~";
+	}
+	
+}
